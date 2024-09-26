c0fb01b2d2b1b4f1fcf119926a78c876115a917b
roma.prd <deyvyalex2006@gmail.com>
2024-09-26 17:27:24 -0300

    Add files via upload

---
+++ calculoCPF/calculocpf.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void calculocpf (){
+
+    int num[20], num1 = 0, num2 = 0, soma1 = 0, resultado1 = 0, soma2 = 0, resultado2 = 0;
+    
+
+    for (int i = 0; i < 9; i++){
+        printf("Digite o %d numero: ", i+1);
+        scanf("%d", &num1);
+        num[i] = num1 * (i+1);
+    }
+
+    for (int i = 0; i < 9; i++){
+        soma1 += num[i];
+    }
+
+    
+    resultado1 = soma1 % 11;
+    system("cls");
+    printf("\nResultado: %d\n", resultado1);
+
+    for (int i = 0; i < 10; i++){
+        num[i] = 0;
+    }
+
+    for (int i = 0; i < 10; i++){
+        printf("Digite o %d numero: ", i+1);
+        scanf("%d", &num2);
+        num[i] = num2 * i;
+    }
+
+     for (int i = 0; i < 10; i++){
+        soma2 += num[i];
+    }
+
+  
+    resultado2 = soma2 % 11;
+    printf("\n%d", resultado1);
+    printf("\n%d", resultado2);
+    return 0;
+}
---
+++ calculoCPF/main1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "calculocpf.c"
+
+int main(){
+    printf("\n--- CALCULADORA DE CPF ---\n");
+    printf("\n");
+   calculocpf();
+
+    return 0;
+}
